77f37cbc74a1a4ed12b81d0d6fab4dc63047ccb0
Zacharie Boisnard <mecazack@gmail.com>
2024-04-21 16:37:55 +0200

    feat: init

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import * as fs from 'fs';
+import { mdToPdf } from 'md-to-pdf';
+
+import md from './discours-19_03_2024.md';
+
+import CSS from './styles/main.scss';
+
+mdToPdf(
+    { content: md },
+    {
+        highlight_style: 'monokai',
+        page_media_type: 'print',
+        pdf_options: {
+            format: 'A4',
+            margin: {
+                left: '20mm',
+                right: '20mm',
+                top: '20mm',
+                bottom: '20mm',
+            },
+        },
+        css: CSS.toString(),
+    },
+)
+    .then((pdf) => {
+        fs.writeFileSync('discours-19.pdf', pdf.content);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+const autoprefixer = require('autoprefixer');
+
+module.exports = (env, argv) => {
+    const mode = argv.mode;
+    const conf = {
+        entry: [path.resolve('src', 'index.ts')],
+        devtool: isProd(mode) ? undefined : 'inline-source-map',
+        plugins: [],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                configFile: path.resolve('tsconfig.json'),
+                            },
+                        },
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.md$/,
+                    use: ['raw-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        //'style-loader',
+                        {
+                            // Translates CSS into CommonJS
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: !isProd(mode),
+                                import: false,
+                            },
+                        },
+                        {
+                            // Compiles Sass to CSS
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: !isProd(mode),
+                                sassOptions: {
+                                    outputStyle: isProd(mode)
+                                        ? 'compressed'
+                                        : 'expanded',
+                                },
+                            },
+                        },
+                        {
+                            // Loader for webpack to process CSS with PostCSS
+                            loader: 'postcss-loader',
+                            options: {
+                                postcssOptions: {
+                                    plugins: [[autoprefixer, {}]],
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(svg|png|jpg|gif|webp)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'imgs',
+                                esModule: false,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(aac|mp3)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'audio',
+                            esModule: false,
+                        },
+                    },
+                },
+                {
+                    test: /\.(webm|mp4)$/,
+                    use: {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'video',
+                            esModule: false,
+                        },
+                    },
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            alias: { '@src': path.resolve('src') },
+        },
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve('build'),
+            clean: true,
+            libraryTarget: 'umd', // ssr
+            globalObject: 'this', // ssr
+        },
+        target: 'node', // ssr
+        externals: [webpackNodeExternals()],
+        watchOptions: {
+            ignored: /node_modules/,
+        },
+    };
+    return conf;
+};
+
+const isProd = (mode) => {
+    return mode === 'production';
+};
