012906127d6aae9fac8b9c433a14da92347d5d9a
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-07-10 22:46:51 +0200

    feat: nix flake to save environment packages

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A node + bun env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }@inputs: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = false;
+      };
+    };
+  in {
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        bun
+        nodejs_22
+      ];
+    };
+
+  };
+}
