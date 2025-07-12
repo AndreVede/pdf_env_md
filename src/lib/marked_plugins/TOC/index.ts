29502a3407c76b3c9c357128ca912723e8d906a7
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-07-12 21:39:38 +0200

    fix: toc break page is absolutely after end

--- src/lib/marked_plugins/TOC/index.ts
+++ src/lib/marked_plugins/TOC/index.ts
@@ -7,7 +7,7 @@ const TOC = (html: string) => {
     const headings = MarkIdHeadingInstance.getHeadingList();
 
     return `
-<ul id="table-of-contents">
+<ul id="table-of-contents" class="page-break-after">
 	${headings.map(({ id, raw, level }) => `<li><a href="#${id}" class="h${level}">${raw}</a></li>`).join('')}
 </ul>
 ${html}`;
